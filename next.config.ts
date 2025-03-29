df4e632395a6a5eeeced152001ba0e1641c17af3
bach9w <78446671+bach9w@users.noreply.github.com>
2025-03-29 20:35:59 +0200

    уп

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	async rewrites() {
+		return [
+			{
+				source: "/api/rankings/extract",
+				destination: "/api/rankings/extract",
+			},
+		];
+	},
 };
 
 export default nextConfig;
